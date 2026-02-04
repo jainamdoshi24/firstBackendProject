c5ba717b16da21f423b92dab02b3edf1f5ebf82e
jainamdoshi24 <doshijainam45@gmail.com>
2026-02-04 16:46:32 +0530

    User and Video Model

---
+++ src/models/video.model.js
@@ -0,0 +1,44 @@
+import mongoose, {Schema} from 'mongoose';
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
+
+const videoSchema = new mongoose.Schema(
+    {
+        videoFile: {
+            type: String,  // Cloudinary URL
+            required: true,
+        },
+        thumbNail: {
+            type: String,  // Cloudinary URL
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: Number, // Duration in seconds
+            required: true,
+        },
+        views: {
+            type: Number,
+            default: 0,
+        },
+        isPublished: {
+            type: Boolean,
+            default: false,
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+        }
+    },
+    { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = mongoose.model('Video', videoSchema);
+
+
